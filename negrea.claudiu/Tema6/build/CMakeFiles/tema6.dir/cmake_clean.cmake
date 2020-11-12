303ee7302c4d760dddad1200075de0f6a835a883
Negrea Claudiu <claudiunegrea7@gmail.com>
2020-11-12 18:14:47 +0200

    cmake

---
+++ negrea.claudiu/Tema6/build/CMakeFiles/tema6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema6.dir/src/main.cpp.o"
+  "tema6"
+  "tema6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
